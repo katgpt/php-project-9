384b608bbf71ab2bb66392380adcb62bbe7fc75f
siteservicehelp <siteservicehelp@gmail.com>
2024-08-31 19:00:54 +0600

    step 2

--- templates/home.php
+++ templates/home.php
@@ -11,57 +11,48 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </head>
 <body class="min-vh-100 d-flex flex-column">
-
     <header class="flex-shrink-0">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark px-2">
-            <div class="container-fluid d-flex justify-content-start px-2">
-                <a class="navbar-brand" href="#">Анализатор страниц</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Главная</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Сайты</a>
-                        </li>
-                    </ul>
-                </div>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark px-3">
+            <a class="navbar-brand" href="/">Анализатор страниц</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/">Главная</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/urls">Сайты</a>
+                    </li>
+
+                </ul>
             </div>
         </nav>
-    </header>
+    </header> 
 
     <main class="flex-grow-1">
-        <div>
-            <div class="container-lg mt-3">
-                <h1></h1>
-                <div class="row">
-                    <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded bg-light p-5">
-                        <h1 class="display-3">Анализатор страниц</h1>
-                        <p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>
-                        <form action="" method="post" class="row" required>
-                            <div class="col-8">
-                                <input type="text"
-                                       name="url[name]"
-                                       value=""
-                                       placeholder="https://www.example.com"
-                                       class="form-control form-control-lg">
-                            </div>
-                            <div class="col-2">
-                                <input class="btn btn-primary btn-lg ms-3 px-5 text-uppercase mx-3"
-                                       type="submit"
-                                       value="Проверить">
-                            </div>
-                        </form>
-                    </div>
+        <div class="container-lg mt-3">
+            <h1></h1>
+            <div class="row">
+                <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded-3 bg-light p-5">
+                    <h1 class="display-3">Анализатор страниц</h1>
+                    <p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>
+                    <form action="/urls" method="post" class="row" required="">
+                        <div class="col-8">
+                        <label for="text" class="visually-hidden">Url для проверки</label>
+                        <input type="text" name="url[name]" value="" class="form-control form-control-lg" placeholder="https://www.example.com">
+                                    </div>
+                        <div class="col-2">
+                            <input type="submit" class="btn btn-primary btn-lg ms-3 px-5 text-uppercase mx-3" value="Проверить">
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </main>
-
+    
     <footer class="border-top py-3 mt-5 flex-shrink-0">
         <div class="container-lg">
             <div class="text-center">
@@ -69,6 +60,7 @@
             </div>
         </div>
     </footer>
+
 
 </body>
 
