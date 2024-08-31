c9b5a695520406dcf8667b4629aa6d5f34921c1e
siteservicehelp <siteservicehelp@gmail.com>
2024-08-31 16:03:48 +0600

    step 2

--- public/index.php
+++ public/index.php
@@ -1,11 +1,20 @@
-<!-- templates/home.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Home</title>
-</head>
-<body>
-    <h1>Hello, <?= htmlspecialchars($name) ?>!</h1>
-</body>
-</html>
+<?php
+
+// Подключение автозагрузки через composer
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\AppFactory;
+use Slim\Views\PhpRenderer;
+
+$app = AppFactory::create();
+
+// Настройка PHP-View
+$container = $app->getContainer();
+$container['view'] = new PhpRenderer('path/to/templates');
+
+// Маршрут для главной страницы
+$app->get('/', function ($request, $response, $args) {
+    return $this->view->render($response, 'home.php', ['name' => 'John']);
+});
+
+$app->run();
---
+++ templates/home.php
@@ -0,0 +1,11 @@
+<!-- templates/home.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Hello, <?= htmlspecialchars($name) ?>!</h1>
+</body>
+</html>
